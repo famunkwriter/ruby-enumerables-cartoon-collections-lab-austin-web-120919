222f8241f2a67a384b8acac5df8b48737d4b8fbd
Bretton B Holmes <famunkwriter@me.com>
2019-11-01 14:30:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def long_planeteer_calls(calls_long)
   calls_long.any? { |n| n.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese)
+  cheese.any? { |n| n.include? === cheese_types}
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
