f6f611d3ab0e2770b28622a9fc40a0b7073629c8
Bretton B Holmes <famunkwriter@me.com>
2019-11-01 14:53:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,6 +14,6 @@ end
 
 def find_the_cheese(cheese)
 cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.index { |n| n == cheese_types }
+  cheese_types.index { |n| n == cheese_types[i] }
   return cheese_types
 end
