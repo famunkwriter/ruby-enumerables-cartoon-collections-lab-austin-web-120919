819d22611eec42762d24ac675b00c6bd50c0b4e9
Bretton B Holmes <famunkwriter@me.com>
2019-11-01 15:14:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,5 +14,5 @@ end
 
 def find_the_cheese(cheese)
 cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese.detect { |n| n % cheese_types.include? == cheese }
+  cheese.include? { |n| n % cheese_types.detect == cheese }
 end
