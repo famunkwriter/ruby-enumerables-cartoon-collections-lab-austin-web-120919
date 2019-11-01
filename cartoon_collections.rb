a0065d6b9581bf2d97b83548ea57aa9bd79548e5
Bretton B Holmes <famunkwriter@me.com>
2019-11-01 14:18:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls_long)
-  calls_long.any? { |calls_long| calls_long > 4}
+  calls_long.any? { |n| n > 4}
 end
 
 def find_the_cheese# code an argument here
